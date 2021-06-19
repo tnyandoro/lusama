1673adaefe19459c5dbcfe7cffa4cbefb0fcabb1
ClaytonSiby <claytonsiby@gmail.com>
2021-06-19 22:12:44 +0200

    Configure devise third-party & authenticate some of the rails app endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create show]
+  devise_for :users
   # resources :order_details, only: %i[new create edit update index destroy]
   resources :orders
   resources :customers, except: %i[index]
   resources :products, except: %i[index]
   resources :categories
+
+  root 'categories#index'
 end
